1ddafeb080fc8d60fa6b583a2f15d0cfeba00b39
Karthik N P <52942938+gh05t-4@users.noreply.github.com>
2019-10-17 21:38:10 +0530

    Add files via upload

---
+++ hex2text.py
@@ -0,0 +1,13 @@
+hexval = input("Enter the hex values: ")
+hexval = hexval.replace(" ","")
+i = 0
+l = len(hexval)
+j = int(l/4)
+text = ""
+while i<j:
+    h = hexval[:4]
+    text = text + chr(int(h,16))
+    hexval = hexval[4:l]
+    i = i + 1
+print("\nText:\n\n" + text)
+print("\n")
